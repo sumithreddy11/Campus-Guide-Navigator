6d6affa3910b6261f5d2a06099352cdc9f7cee91
Sumith Mudireddy <msraseb@gmail.com>
2026-09-17 22:30:43 +0530

    Add campus locations with coordinates and radius

--- raspberry_pi/campus_map.py
+++ raspberry_pi/campus_map.py
@@ -1,1 +1,80 @@
 
+CAMPUS_LOCATIONS = {
+    "Main Gate": {
+        "latitude": 12.895321088271368,
+        "longitude": 77.67541736863429,
+        "radius": 12.0
+    },
+
+    "Sideward Gateway": {
+        "latitude": 12.895392988713244,
+        "longitude": 77.67495066425175,
+        "radius": 10.0
+    },
+
+    "A-Block": {
+        "latitude": 12.89463934025986,
+        "longitude": 77.6746945132899,
+        "radius": 18.0
+    },
+
+    "B-Block": {
+        "latitude": 12.894485734267496,
+        "longitude": 77.67458722492849,
+        "radius": 18.0
+    },
+
+    "C-Block": {
+        "latitude": 12.894126847725763,
+        "longitude": 77.6745391634929,
+        "radius": 16.0
+    },
+
+    "D-Block": {
+        "latitude": 12.89397128048859,
+        "longitude": 77.67484962918999,
+        "radius": 16.0
+    },
+
+    "Canteen": {
+        "latitude": 12.89392617904453,
+        "longitude": 77.67547927775851,
+        "radius": 14.0
+    },
+
+    "1st-Turn": {
+        "latitude": 12.894103970212063,
+        "longitude": 77.67556779064012,
+        "radius": 8.0
+    },
+
+    "Electrical Lab": {
+        "latitude": 12.89409612648748,
+        "longitude": 77.67580918945889,
+        "radius": 12.0
+    },
+
+    "Left Turn": {
+        "latitude": 12.894328823543914,
+        "longitude": 77.6759520170908,
+        "radius": 8.0
+    },
+
+    "Path: E-Block to Hostel": {
+        "latitude": 12.89464845482911,
+        "longitude": 77.67608545698886,
+        "radius": 10.0
+    },
+
+    "E-Block": {
+        "latitude": 12.89501514788762,
+        "longitude": 77.67585478700951,
+        "radius": 20.0
+    },
+
+    "End Point": {
+        "latitude": 12.895217776620532,
+        "longitude": 77.67546050228253,
+        "radius": 10.0
+    }
+}
