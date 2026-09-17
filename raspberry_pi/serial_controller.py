909d5f6dd86a3ba8b8e6c50a5ef073e2be25eb76
Sumith Mudireddy <msraseb@gmail.com>
2026-09-17 22:32:12 +0530

    Update serial_controller.py

--- raspberry_pi/serial_controller.py
+++ raspberry_pi/serial_controller.py
@@ -1,1 +1,48 @@
 
+import serial
+import time
+
+
+class MotorController:
+
+    def __init__(
+        self,
+        port="/dev/ttyACM0",
+        baudrate=115200
+    ):
+
+        self.serial = serial.Serial(
+            port,
+            baudrate,
+            timeout=1
+        )
+
+        time.sleep(2)
+
+    def send_command(self, command):
+
+        command = command.upper().strip()
+
+        self.serial.write(
+            (command + "\n").encode("utf-8")
+        )
+
+        print("Arduino command:", command)
+
+    def forward(self):
+        self.send_command("FORWARD")
+
+    def backward(self):
+        self.send_command("BACKWARD")
+
+    def left(self):
+        self.send_command("LEFT")
+
+    def right(self):
+        self.send_command("RIGHT")
+
+    def stop(self):
+        self.send_command("STOP")
+
+    def close(self):
+        self.serial.close()
